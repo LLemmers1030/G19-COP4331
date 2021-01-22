36f9f10e7409aed519feaa717be17d171c848a1a
Samantha Perez <54879396+samantha238@users.noreply.github.com>
2021-01-21 22:03:38 -0500

    Add files via upload

--- www/html/poop_ninja1/poop_ninja1/script.js
+++ www/html/poop_ninja1/poop_ninja1/script.js
@@ -1,18 +1,47 @@
-'use strict'
+var urlBase = 'http://poop21.ninja/LAMPAPI';
+var extension = 'php';
 
-function login() {
-	
-	var username = document.getElementById("loginUsername").value;
+
+var userId = 0;
+var firstName = "";
+var lastName = "";
+
+
+function doLogin() {
+
+	userId = 0;
+	firstName = "";
+	lastName = "";
+
+	var login = document.getElementById("loginUsername").value;
 	var password = document.getElementById("loginPassword").value;
 
-	
-	
-	if (username === "ninja" && password === "COP4331") {
-        alert("Login Successful");
-		//window.location.href = "color.html";
-		window.open("color.html");
-    } else {
-        alert("Login Unsuccessful");
-		location.reload();
-    }
+	document.getElementById("loginResult").innerHTML = "";
+
+	var jsonPayload = '{"Login" : "' + login + '", "Password" : "' + password + '"}';
+	var url = urlBase + '/Login.' + extension;;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try {
+		xhr.send(jsonPayload);
+		var jsonObject = JSON.parse( xhr.responseText );
+		userId = jsonObject.id;
+
+		if( userId < 1 ) {
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+		}
+
+		firstName = jsonObject.firstName;
+		lastName = jsonObject.lastName;
+
+		//saveCookie();
+
+		window.location.href = "https://www.youtube.com";
+	}
+	catch(err) {
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
 }
