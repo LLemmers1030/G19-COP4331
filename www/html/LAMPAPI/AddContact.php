5fa3b5c7626281f044dd448e926b07288f249589
Scientificmagic <thechanman.potato@gmail.com>
2021-01-22 13:12:49 -0500

    +AddContact.php

---
+++ www/html/LAMPAPI/AddContact.php
@@ -0,0 +1,51 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	// Retrival variables
+	$adderID = $inData["AdderID"];
+	$firstName = $inData["FirstName"];
+	$lastName = $inData["LastName"];
+	$email = $inData["Email"];
+	$phone = $inData["Phone"];
+	
+	$conn = new mysqli("localhost", "Jonin", "shuriken", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		//$sql = "INSERT INTO users (email, password) VALUES (?,?)";
+		//$stmt= $conn->prepare($sql);
+		//$stmt->bind_param("ss", $email, $password);
+		//$stmt->execute();
+
+		$sql = "insert into Contacts (AdderID,FirstName,LastName,Email,Phone) VALUES (" . $adderID . ",'" . $firstName . "','" . $lastName . "','" . $email . "','" . $phone . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
