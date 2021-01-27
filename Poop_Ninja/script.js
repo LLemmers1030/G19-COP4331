d098a0b46ca2fdbad5c21a83daccf7cef6e7cc40
Scientificmagic <thechanman.potato@gmail.com>
2021-01-26 21:38:25 -0500

    removed folder

--- Poop_Ninja/script.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const login = document.querySelector('.login').addEventListener("click", function(){
-    console.log("We logged in!")
-    alert("Login Successful");
-    window.open("register.html");
-});
