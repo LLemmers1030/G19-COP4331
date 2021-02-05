e831a51005eb5ecc1ed6fd66bea915fef5b35e83
Samantha Perez <samanthaaaperezzz@gmail.com>
2021-02-05 05:54:07 -0500

    minor table changes, restructured some JS functions

--- new_ninja/js/script.js
+++ new_ninja/js/script.js
@@ -7,8 +7,9 @@ var userId = 0;
 var firstName = "";
 var lastName = "";
 
-$('button[data-bs-dismiss="modal"]').click(function(){
-	$(this).closest('.modal').modal('hide'); 
+
+$('button[data-bs-dismiss="modal"]').click(function () {
+	$(this).closest('.modal').modal('hide');
 });
 
 
@@ -23,42 +24,46 @@ function doLogin() { // working
 
 	document.getElementById("loginResult").innerHTML = "";
 
-	var jsonPayload = '{"Login" : "' + login + '", "Password" : "' + password + '"}';
+	//var jsonPayload = '{"Login" : "' + login + '", "Password" : "' + password + '"}';
 	var url = urlBase + '/Login.' + extension;
-	
+
 	fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: jsonPayload // change to JSON.stringify ???
+		//body: jsonPayload // change to JSON.stringify ???
+		body: JSON.stringify({
+			Login: login,
+			Password: password
+		})
 	}).then(res => {
 		return res.json()
-	  })
-	  .then(data => {
-		userId = data.id;
+	})
+		.then(data => {
+			userId = data.id;
 
-		if (userId < 1 || userId == null) {
-			document.getElementById("loginResult").innerHTML = "Invalid User/Password combination";
-			return;
-		}
+			if (userId < 1 || userId == null) {
+				document.getElementById("loginResult").innerHTML = "Invalid User/Password combination";
+				return;
+			}
 
-		firstName = data.firstName;
-		lastName = data.lastName;
+			firstName = data.firstName;
+			lastName = data.lastName;
 
-		saveCookie();
+			saveCookie();
 
-		window.location.href = "search.html";
-	  })
-	  .catch((err) => {
-		document.getElementById("loginResult").innerHTML = err.message;
-	  });
+			window.location.href = "search.html";
+		})
+		.catch((err) => {
+			document.getElementById("loginResult").innerHTML = err.message;
+		});
 }
 
 function saveCookie() { // working
 	var minutes = 20;
 	var date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
+	date.setTime(date.getTime() + (minutes * 60 * 1000));
 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
@@ -69,18 +74,18 @@ function readCookie() { // working
 	for (var i = 0; i < splits.length; i++) {
 		var thisOne = splits[i].trim();
 		var tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" ) {
+		if (tokens[0] == "firstName") {
 			firstName = tokens[1];
 		}
-		else if( tokens[0] == "lastName" ) {
+		else if (tokens[0] == "lastName") {
 			lastName = tokens[1];
 		}
-		else if( tokens[0] == "userId" ) {
-			userId = parseInt( tokens[1].trim() );
+		else if (tokens[0] == "userId") {
+			userId = parseInt(tokens[1].trim());
 		}
 	}
-	
-	if ( userId < 0 ) {
+
+	if (userId < 0) {
 		window.location.href = "index.html";
 	}
 	else {
@@ -109,36 +114,42 @@ function doRegister() { // not implemented
 
 	document.getElementById("registerResult").innerHTML = "";
 
-	var jsonPayload = '{"FirstName" : "' + first + '", "LastName" : "' + last + '", "Login" : "' + login + '", "Password" : "' + password + '"}';
+	//var jsonPayload = '{"FirstName" : "' + first + '", "LastName" : "' + last + '", "Login" : "' + login + '", "Password" : "' + password + '"}';
 	var url = urlBase + '/Register.' + extension;
-	
+
 	fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: jsonPayload // change to JSON.stringify ???
+		//body: jsonPayload // change to JSON.stringify ???
+		body: JSON.stringify({
+			FirstName: first,
+			LastName: last,
+			Login: login,
+			Password: password
+		})
 	}).then(res => {
 		return res.json()
-	  })
-	  .then(data => {
-		userId = data.id;
+	})
+		.then(data => {
+			userId = data.id;
 
-		if (userId < 1 || userId == null) {
-			document.getElementById("registerResult").innerHTML = "Registration was unsuccessful";
-			return;
-		}
+			if (userId < 1 || userId == null) {
+				document.getElementById("registerResult").innerHTML = "Registration was unsuccessful";
+				return;
+			}
 
-		firstName = data.firstName;
-		lastName = data.lastName;
+			firstName = data.firstName;
+			lastName = data.lastName;
 
-		saveCookie();
+			saveCookie();
 
-		window.location.href = "search.html";
-	  })
-	  .catch((err) => {
-		document.getElementById("registerResult").innerHTML = err.message;
-	  });
+			window.location.href = "search.html";
+		})
+		.catch((err) => {
+			document.getElementById("registerResult").innerHTML = err.message;
+		});
 }
 
 function addContact() { // working
@@ -150,39 +161,47 @@ function addContact() { // working
 
 	document.getElementById("addResult").innerHTML = "";
 
-	var jsonPayload = '{"AdderID" : "' + userId + '", "FirstName" : "' + first + '", "LastName" : "' + last + '", "Email" : "' + email + '", "Phone" : "' + phone + '"}';
-
+	//var jsonPayload = '{"AdderID" : "' + userId + '", "FirstName" : "' + first + '", "LastName" : "' + last + '", "Email" : "' + email + '", "Phone" : "' + phone + '"}';
 	var url = urlBase + '/AddContact.' + extension;
-	
+
 	fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: jsonPayload // change to JSON.stringify ???
+		//body: jsonPayload // change to JSON.stringify ???
+		body: JSON.stringify({
+			AdderID: userId,
+			FirstName: first,
+			LastName: last,
+			Email: email,
+			Phone: phone
+		})
 	}).then(res => {
 		return res.json()
-	  })
-	  .then(data => {
-		  // can we return contact ID as well ?
-		addContactErr = data.err; 
+	})
+		.then(data => {
+			// can we return contact ID as well ?
+			addContactErr = data.err;
 
-		if (addContactErr == null) {
-			document.getElementById("addResult").innerHTML = "Contact was successfully added";
-		}
-		else {
-			document.getElementById("addResult").innerHTML = "Contact was not added";
-			return;
-		}
+			if (addContactErr == null) {
+				document.getElementById("addResult").innerHTML = "Contact was successfully added";
+			}
+			else {
+				document.getElementById("addResult").innerHTML = "Contact was not added";
+				return;
+			}
 
-		// do we want to give option to add more contacts instead?
-		// so user doesn't have to keep clicking add user button after page refresh
-		//window.location.href = "search.html";
+			// reset fields
+			document.getElementById("addFirst").value = "";
+			document.getElementById("addLast").value = "";
+			document.getElementById("addEmail").value = "";
+			document.getElementById("addPhone").value = "";
 
-	  })
-	  .catch((err) => {
-		document.getElementById("addResult").innerHTML = err.message;
-	  });
+		})
+		.catch((err) => {
+			document.getElementById("addResult").innerHTML = err.message;
+		});
 }
 
 function searchContact() { // working
@@ -191,112 +210,28 @@ function searchContact() { // working
 
 	document.getElementById("searchResult").innerHTML = "";
 
-	var jsonPayload = '{"AdderID" : "' + userId + '", "Search" : "' + search + '"}';
+	//var jsonPayload = '{"AdderID" : "' + userId + '", "Search" : "' + search + '"}';
 	var url = urlBase + '/SearchContact.' + extension;
-	
+
 	fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: jsonPayload // change to JSON.stringify ???
+		//body: jsonPayload // change to JSON.stringify ???
+		body: JSON.stringify({
+			AdderID: userId,
+			Search: search
+		})
 	}).then(res => {
 		return res.json()
-	  })
-	  .then(data => {
-		  numContacts = data.count;
-
-		if (numContacts < 1 || numContacts == null) {
-			document.getElementById("searchResult").innerHTML = "No contacts were found";
-			return;
-		}
-
-		// below is code to make search results table
-
-		// header titles
-		var col = [];
-		for (var i = 0; i < numContacts; i++) {
-			for (var key in data.results[i]) {
-				if (col.indexOf(key) == -1) {
-					col.push(key);
-				}
-			}
-		}
-
-		// extra column titles (for edit and remove)
-		col.push("Edit");
-		col.push("Delete");
-
-		// create dynamic table
-		var table = document.createElement("table");
-		table.className = "table table-dark table-striped";
-
-		// create header row from titles
-		var tr = table.insertRow(-1);
-		for (var i = 0; i < col.length; i++) {
-			var th = document.createElement("th");
-			th.innerHTML = col[i];
-			tr.appendChild(th);
-		}
-
-		// add JSON data to table
-		for (var i = 0; i < numContacts; i++) {
-			tr = table.insertRow(-1);
-			for (var j = 0; j < col.length; j++) {
-				if (j == col.length - 1) { // remove button
-					var td = tr.insertCell(-1);
-					var rmBtn = document.createElement('button');
-					rmBtn.type = "button";
-					rmBtn.className = "btn btn-primary";
-					rmBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-					//rmBtn.innerHTML = '<i class="fas fa-user-times"></i>';
-					rmBtn.onclick = function() {
-						$('#deletemodal').modal('show');
-						var index = $(this).closest('tr').index() - 1;
-                        var contactID = data.results[index].ID;
-						$('.modal-body #delete_id').val(contactID);
-					}
-					td.appendChild(rmBtn);
-				}
-				else if (j == col.length - 2) { // edit button
-					var td = tr.insertCell(-1);
-					var editBtn = document.createElement('button');
-					editBtn.type = "button";
-					editBtn.className = "btn btn-primary";
-					editBtn.innerHTML = '<i class="fas fa-user-edit"></i>';
-
-					editBtn.onclick = function() {
-						$('#editmodal').modal('show');
-						var index = $(this).closest('tr').index() - 1;
-                        var contactID = data.results[index].ID;
-						$('.modal-body #update_id').val(contactID);
-
-						// fills in text boxes
-						$('#updateFirst').val(data.results[index].FirstName);
-						$('#updateLast').val(data.results[index].LastName);
-						$('#updateEmail').val(data.results[index].Email);
-						$('#updatePhone').val(data.results[index].Phone);
-					}
-
-					td.appendChild(editBtn);
-				}
-				else {
-					var td = tr.insertCell(-1);
-					td.innerHTML = data.results[i][col[j]];
-				}
-			}
-		}
-
-		// add table to container
-		var divContainer = document.getElementById("table-id");
-		divContainer.innerHTML = "";
-		divContainer.appendChild(table);
-		
-		  getPagination('#table-id');
-	  })
-	  .catch((err) => {
-		document.getElementById("searchResult").innerHTML = err.message;
-	  });
+	})
+		.then(data => {
+			createTable(data);
+		})
+		.catch((err) => {
+			document.getElementById("searchResult").innerHTML = err.message;
+		});
 }
 
 function updateContact() { // working
@@ -309,34 +244,41 @@ function updateContact() { // working
 
 	document.getElementById("updateResult").innerHTML = "";
 
-	var jsonPayload = '{"AdderID" : "' + userId + '", "ID" : "' + ID2update + '", "FirstName" : "' + first + '", "LastName" : "' + last + '", "Email" : "' + email + '", "Phone" : "' + phone + '"}';
-
+	//var jsonPayload = '{"AdderID" : "' + userId + '", "ID" : "' + ID2update + '", "FirstName" : "' + first + '", "LastName" : "' + last + '", "Email" : "' + email + '", "Phone" : "' + phone + '"}';
 	var url = urlBase + '/UpdateContact.' + extension;
-	
+
 	fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: jsonPayload // change to JSON.stringify ???
+		//body: jsonPayload // change to JSON.stringify ???
+		body: JSON.stringify({
+			AdderID: userId,
+			ID: ID2update,
+			FirstName: first,
+			LastName: last,
+			Email: email,
+			Phone: phone
+		})
 	}).then(res => {
 		return res.json()
-	  })
-	  .then(data => {
-		updateContactErr = data.error; 
+	})
+		.then(data => {
+			updateContactErr = data.error;
 
-		if (updateContactErr == null) {
-			document.getElementById("updateResult").innerHTML = "Contact was successfully updated";
-		}
-		else {
-			document.getElementById("updateResult").innerHTML = "Contact was not updated";
-			return;
-		}
+			if (updateContactErr == null) {
+				document.getElementById("updateResult").innerHTML = "Contact was successfully updated";
+			}
+			else {
+				document.getElementById("updateResult").innerHTML = "Contact was not updated";
+				return;
+			}
 
-	  })
-	  .catch((err) => {
-		document.getElementById("updateResult").innerHTML = err.message;
-	  });
+		})
+		.catch((err) => {
+			document.getElementById("updateResult").innerHTML = err.message;
+		});
 }
 
 function removeContact() { // working
@@ -345,96 +287,177 @@ function removeContact() { // working
 
 	document.getElementById("deleteResult").innerHTML = "";
 
-	var jsonPayload = '{"AdderID" : "' + userId + '", "ID" : "' + ID2delete + '"}';
-
+	//var jsonPayload = '{"AdderID" : "' + userId + '", "ID" : "' + ID2delete + '"}';
 	var url = urlBase + '/DeleteContact.' + extension;
-	
+
 	fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: jsonPayload // change to JSON.stringify ???
+		//body: jsonPayload // change to JSON.stringify ???
+		body: JSON.stringify({
+			AdderID: userId,
+			ID: ID2delete
+		})
 	}).then(res => {
 		return res.json()
-	  })
-	  .then(data => {
-		deleteContactErr = data.error; 
+	})
+		.then(data => {
+			deleteContactErr = data.error;
 
-		if (deleteContactErr == null) {
-			document.getElementById("deleteResult").innerHTML = "Contact was successfully deleted";
-		}
-		else {
-			document.getElementById("deleteResult").innerHTML = "Contact was not deleted";
-			return;
-		}
+			if (deleteContactErr == null) {
+				document.getElementById("deleteResult").innerHTML = "Contact was successfully deleted";
+			}
+			else {
+				document.getElementById("deleteResult").innerHTML = "Contact was not deleted";
+				return;
+			}
 
-	  })
-	  .catch((err) => {
-		document.getElementById("deleteResult").innerHTML = err.message;
-	  });
+		})
+		.catch((err) => {
+			document.getElementById("deleteResult").innerHTML = err.message;
+		});
 }
 
+function createTable(data) {
+	var tableBody = document.getElementById("table-data");
+	$("#table-data tr").remove();
 
+	// below is code to make search results table
+	numContacts = data.count;
+	if (numContacts == 0) {
+		var tr = tableBody.insertRow(-1);
+		tr.className = "no-contacts";
+		var td = tr.insertCell(-1);
+		td.innerHTML = "No contacts were found.";
+		td.colSpan = "10";
+	}
+
+	// header titles
+	var col = [];
+	for (var i = 0; i < numContacts; i++) {
+		for (var key in data.results[i]) {
+			if (col.indexOf(key) == -1) {
+				col.push(key);
+			}
+		}
+	}
+
+	// add JSON data to table
+	for (var i = 0; i < numContacts; i++) {
+		var tr = tableBody.insertRow(-1);
+		for (var j = 0; j < col.length + 2; j++) {
+			if (j == col.length + 1) { // remove button
+				var td = tr.insertCell(-1);
+				var rmBtn = document.createElement('button');
+				rmBtn.type = "button";
+				rmBtn.className = "btn btn-primary";
+				rmBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+				//rmBtn.innerHTML = '<i class="fas fa-user-times"></i>';
+				rmBtn.onclick = function () {
+					$('#deletemodal').modal('show');
+					var index = $(this).closest('tr').index() - 1;
+					var contactID = data.results[index].ID;
+					$('.modal-body #delete_id').val(contactID);
+				}
+				td.appendChild(rmBtn);
+			}
+			else if (j == col.length) { // edit button
+				var td = tr.insertCell(-1);
+				var editBtn = document.createElement('button');
+				editBtn.type = "button";
+				editBtn.className = "btn btn-primary";
+				editBtn.innerHTML = '<i class="fas fa-user-edit"></i>';
+
+				editBtn.onclick = function () {
+					$('#editmodal').modal('show');
+					var index = $(this).closest('tr').index() - 1;
+					var contactID = data.results[index].ID;
+					$('.modal-body #update_id').val(contactID);
+
+					// fills in text boxes
+					$('#updateFirst').val(data.results[index].FirstName);
+					$('#updateLast').val(data.results[index].LastName);
+					$('#updateEmail').val(data.results[index].Email);
+					$('#updatePhone').val(data.results[index].Phone);
+				}
+
+				td.appendChild(editBtn);
+			}
+			else {
+				var td = tr.insertCell(-1);
+				td.innerHTML = data.results[i][col[j]];
+			}
+		}
+	}
+
+	getPagination('#table-id');
+}
 
 
 function getPagination(table) {
 
-    $('.pagination').html('');
-    var trnum = 0;	
-    var maxRows = 5;
+	$('.pagination').html('');
+	var trnum = 0;
+	var maxRows = 5;
 
-    var totalRows = $(table + ' tbody tr').length;
-    $(table + ' tr:gt(0)').each(function () {	
-        trnum++;	
-        if (trnum > maxRows) {	
+	var totalRows = $(table + ' tbody tr').length;
 
-            $(this).hide();			
-        } if (trnum <= maxRows) { $(this).show(); }
-    });									
-    if (totalRows > maxRows) {				
-        var pagenum = Math.ceil(totalRows / maxRows);	 
-        //	numbers of pages 
-        for (var i = 1; i <= pagenum;) {
-            $('.pagination').append('<li class="page-item" data-page="' + i + '">\
+	$(table + ' tr:gt(0)').each(function () {
+		trnum++;
+		if (trnum > maxRows) {
+
+			$(this).hide();
+		} if (trnum <= maxRows) { $(this).show(); }
+	});
+	if (totalRows > maxRows) {
+		var pagenum = Math.ceil(totalRows / maxRows);
+		//	numbers of pages 
+		for (var i = 1; i <= pagenum;) {
+			$('.pagination').append('<li class="page-item" data-page="' + i + '">\
 								      <span>'+ i++ + '<span class="sr-only page-link">(current)</span></span>\
 								    </li>').show();
-        }	
-    } 							
-    $('.pagination li:first-child').addClass('active'); 
+		}
+	}
+	$('.pagination li:first-child').addClass('active');
 
 
-    showing_rows_count(maxRows, 1, totalRows);
+	showing_rows_count(maxRows, 1, totalRows, table);
 
-    $('.pagination li').on('click', function (e) {	
-        e.preventDefault();
-        var pageNum = $(this).attr('data-page');	
-        var trIndex = 0;						
-        $('.pagination li').removeClass('active');	
-        $(this).addClass('active');				
+	$('.pagination li').on('click', function (e) {
+		e.preventDefault();
+		var pageNum = $(this).attr('data-page');
+		var trIndex = 0;
+		$('.pagination li').removeClass('active');
+		$(this).addClass('active');
 
+		showing_rows_count(maxRows, pageNum, totalRows, table);
 
-        showing_rows_count(maxRows, pageNum, totalRows);
-
-
-
-        $(table + ' tr:gt(0)').each(function () {	
-            trIndex++;						
-            if (trIndex > (maxRows * pageNum) || trIndex <= ((maxRows * pageNum) - maxRows)) {
-                $(this).hide();
-            } else { $(this).show(); } 			
-        }); 						
-    });	
+		$(table + ' tr:gt(0)').each(function () {
+			trIndex++;
+			if (trIndex > (maxRows * pageNum) || trIndex <= ((maxRows * pageNum) - maxRows)) {
+				$(this).hide();
+			} else { $(this).show(); }
+		});
+	});
 
 }
 
 //ROWS SHOWING FUNCTION
-function showing_rows_count(maxRows, pageNum, totalRows) {
-    var end_index = maxRows * pageNum;
-    var start_index = ((maxRows * pageNum) - maxRows) + parseFloat(1);
-    if (end_index > totalRows) {
-        end_index = totalRows;
-    }
-    var string = 'Showing ' + start_index + ' to ' + end_index + ' of ' + totalRows + ' entries';
-    $('.rows_count').html(string);
+function showing_rows_count(maxRows, pageNum, totalRows, table) {
+	var end_index = maxRows * pageNum;
+	var start_index = ((maxRows * pageNum) - maxRows) + parseFloat(1);
+	if (totalRows == 1) {
+		var flag = $(table + " tr:gt(0)").hasClass("no-contacts");
+		if (flag) {
+			$('.rows_count').html("");
+			return;
+		}
+	}
+	if (end_index > totalRows) {
+		end_index = totalRows;
+	}
+	var string = 'Showing ' + start_index + ' to ' + end_index + ' of ' + totalRows + ' entries';
+	$('.rows_count').html(string);
 }
